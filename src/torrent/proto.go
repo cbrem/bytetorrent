394855ab5235a982273c2074d6b65916168859f7
cbrem <connorbrem@gmail.com>
2014-04-22 18:10:01 -0400

    Updated torrent proto with ID and ChunkID structs.

--- src/torrent/proto.go
+++ src/torrent/proto.go
@@ -1,20 +1,27 @@
 package torrent
 
-const (
-    DEFAULT_CHUNK_SIZE int 1000 // Number of bytes per chunk
-)
-
 // Information about one node in a tracker.
-type TrackerNode {
+type TrackerNode struct {
     HostPort string
+}
+
+// A Key which uniquely identifies a Torrent.
+// It has the form <name, file_hash>.
+type ID struct {
+    Name string // A human-readable name for this Torrent
+    Hash []byte // The SHA-1 hash of the file associated with the Torrent
+}
+
+// An identifier for a chunk within a torrent.
+type ChunkID struct {
+    ID
+    ChunkNum int
 }
 
 // A deserialized .torrent file.
 // Contains information about how to fetch 
 type Torrent struct {
-    Name string // A human-readable name
-    ID string // A identifier that is unique for the tracker 
-    Hash []byte // The SHA-1 hash of the associated file
+    ID
     ChunkHashes [][]byte
     TrackerNodes []TrackerNode // The nodes in the tracker with which this torrent is registered
     ChunkSize int
