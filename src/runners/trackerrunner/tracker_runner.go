97d3d74c0e29b2305ac3deb384c540966e0a362f
billy <wtwood@andrew.cmu.edu>
2014-04-29 21:09:37 -0400

    Tracker Runner

--- src/runners/trackerrunner/tracker_runner.go
+++ src/runners/trackerrunner/tracker_runner.go
@@ -12,20 +12,21 @@ import (
 var (
 	USAGE string = strings.Join([]string{
 		"Usage:",
-		"\t<program_name> <tracker port> <tracker numNodes> <optional master hostPort>",
+		"\t<program_name> <tracker port> <tracker numNodes> <tracker nodeID> <optional master hostPort>",
 		""}, "\n")
 )
 
 func main() {
 	// Exit if correct args are not supplied.
-	if len(os.Args) != 4 && len(os.Args) != 3 {
+	if len(os.Args) != 5 && len(os.Args) != 4 {
 		fmt.Println(USAGE)
 		return
 	}
-	port := strconv.Atoi(os.Args[1])
-	numNodes := strconv.Atoi(os.Args[2])
+	port, _ := strconv.Atoi(os.Args[1])
+	numNodes, _ := strconv.Atoi(os.Args[2])
+	nodeID, _ := strconv.Atoi(os.Args[3])
 	var master string
-	if len(os.Args) == 4 {
+	if len(os.Args) == 5 {
 		master = os.Args[3]
 	} else {
 		master = ""
@@ -35,7 +36,7 @@ func main() {
 	if _, err := tracker.NewTrackerServer(master, numNodes, port, nodeID); err != nil {
 		fmt.Println("Failed to start tracker", err)
 	} else {
-		fmt.Println("Started tracker with hostPort =", hostPort)
+		fmt.Println("Started tracker with hostPort =", port)
 	}
 
 	// Block forever.
